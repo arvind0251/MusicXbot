9c08bd96a1127298b7cd1d3c88c2e2567125e188
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2024-05-19 11:38:52 +0530

    Update play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -47,21 +47,21 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 40:
-        bar = "◉——————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 10 < umm < 20:
-        bar = "—◉—————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 20 < umm < 30:
-        bar = "——◉————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 30 <= umm < 40:
-        bar = "———◉———————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 40 <= umm < 50:
-        bar = "————◉——————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 50 <= umm < 60:
-        bar = "——————◉————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 50 <= umm < 70:
-        bar = "———————◉———"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     else:
-        bar = "——————————◉"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
 
     buttons  = [
 
@@ -321,21 +321,21 @@ def stream_markup_timer2(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 40:
-        bar = "◉——————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 10 < umm < 20:
-        bar = "—◉—————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 20 < umm < 30:
-        bar = "——◉————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 30 <= umm < 40:
-        bar = "———◉———————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 40 <= umm < 50:
-        bar = "————◉——————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 50 <= umm < 60:
-        bar = "——————◉————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 50 <= umm < 70:
-        bar = "———————◉———"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     else:
-        bar = "——————————◉"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
 
 
     buttons = [
@@ -489,21 +489,21 @@ def panel_markup_4(_, vidid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 40:
-        bar = "◉——————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 10 < umm < 20:
-        bar = "—◉—————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 20 < umm < 30:
-        bar = "——◉————————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 30 <= umm < 40:
-        bar = "———◉———————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 40 <= umm < 50:
-        bar = "————◉——————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 50 <= umm < 60:
-        bar = "——————◉————"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     elif 50 <= umm < 70:
-        bar = "———————◉———"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
     else:
-        bar = "——————————◉"
+        bar = "Ⓡ▰▱▱▱▱▱▱▱▰"
         
     buttons = [
         [
